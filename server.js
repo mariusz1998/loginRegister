fd73e8c9e5ad7942e6d0a0c65b03c3d0e9a45777
Mariusz Drynda <mariusz1998@wp.pl>
2020-11-30 17:23:00 +0100

    sprawdzenie czy git łapie

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
           id: Date.now().toString(),
           name: req.body.name,
           email: req.body.email,
-          password: hashedPassword //haszowanie hasła
+          password: hashedPassword //haszowanie hasłaaa
         })
         res.redirect('/login')
       } catch{
