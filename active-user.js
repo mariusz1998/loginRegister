10efda97e5439d0ec8bba22721e5bc6be7e9dc68
Mariusz Drynda <mariusz1998@wp.pl>
2020-12-11 16:53:09 +0100

    działajaca aktywacja userów

--- active-user.js
+++ active-user.js
@@ -20,18 +20,16 @@ function activeUser(app,checkAuthenticated,sessionNeo) {
   })
   app.get('/users/activates',checkAuthenticated,(req, res)=>{
   var obj = JSON.parse(req.query.JSONFrom);
-// console.log( obj)
-  console.log( obj["usersToAdd"][0])  //pobranie 0 użytkonika 
-    var allUsersEmails = []
+ // console.log( obj["usersToAdd"][0])  //pobranie 0 użytkonika 
+ 
+    var params = {"email": []};
+    obj["usersToAdd"].forEach((item)=>{
+      params.email.push(item)
+    })
   sessionNeo
-  .run('MATCH (n:User{active:false}) RETURN (n)') 
-  .then(function(result){
-    result.records.forEach(function(record){
-      allUsersEmails.push(record._fields[0].identity.low+" "+record._fields[0].properties.email)
-
-    });
-    console.log(allUsersEmails+"Jestem")
-
+  .run('MATCH (n:User) WHERE n.email IN $email  SET n.active = true',params) 
+  .then(function(){
+    res.render('activateUsers.ejs')
 })
  });
 }
