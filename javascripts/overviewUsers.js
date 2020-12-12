8a0b81db73299ddfd3c34e6121385de61966e14e
Mariusz Drynda <mariusz1998@wp.pl>
2020-12-12 18:09:55 +0100

    walka z kalendarzem przy set Admin

--- javascripts/overviewUsers.js
+++ javascripts/overviewUsers.js
@@ -14,35 +14,55 @@ function overviewUsers(app,checkAuthenticated,sessionNeo)
             allUsers.push(record._fields[0].identity.low+") "+record._fields[0].properties.email 
             + " "+ record._fields[0].properties.firstName + " "+ record._fields[0].properties.lastName )
           });
-        res.render('activate-users-list.ejs',{users: allUsers})
+        res.render('statisticsUserList.ejs',{users: allUsers})
       })
     });
 
-    app.get('/users/setAdmin',checkAuthenticated,(req, res)=>{
-
-      req.session.usersChoose=[]
-    //  console.log(req.query.JSONFrom)
+    app.get('/user/showStatistics',checkAuthenticated,(req, res)=>{
       var obj = JSON.parse(req.query.JSONFrom);
-   //   console.log( obj["usersToAdd"].length)
-   //   console.log( obj["usersToAdd"][0])  //pobranie 0 użytkonika 
-    //  console.log( obj["usersToAdd"][0]["id"]);
-  
-        obj["usersToAdd"].forEach((item)=>{
+    var  user = new Object();
+    user.email = obj["userToAdd"][0]["email"]
+    user.id =  obj["userToAdd"][0]["id"]
+    user.lastName =  obj["userToAdd"][0]["lastName"]
+    user.firstName =  obj["userToAdd"][0]["firstName"]
+      sessionNeo
+      .run('MATCH (u:User{email:$emailParam}) OPTIONAL MATCH (u)-[:ADMIN]-(a:Admin) RETURN a'
+      ,{emailParam: obj["userToAdd"][0]["email"]}) 
+      .then(function(result){
+        if(result.records[0].get('a')!=null){
+          user.adminDayStart = result.records[0].get('a').properties.startDay;
+          user.adminDayEnd = result.records[0].get('a').properties.endDay;
+          user.admin=true
+        }
+        else
+        user.admin = false
+        req.session.selectUser=user;
+        res.render('statisticsUser.ejs',{user: user})
+        //console.log(user.adminDayEnd)
+      })
+     });  
 
-     var  user = new Object();
-       user.id  =item["id"] 
-       user.lastName = item["lastName"]
-       user.firstName = item["firstName"]
-       user.email = item["email"]
-          req.session.usersChoose.push(user)
-        })
-        console.log( req.session.usersChoose)
-    //  sessionNeo
-    //  .run('MATCH (n:User) WHERE n.email IN $email  SET n.active = true',params) 
-    //  .then(function(){
-    //    res.redirect('/users/activate/success');
-       //res.render('activateUsers.ejs')
-  //  })
-     });
+     app.post('/user/setAdmin',checkAuthenticated,(req, res)=>{
+     // req.session.selectUser
+     //pobierz dzisiejsza date
+    res.render('setAdminDateRange.ejs',{user: req.session.selectUser})
+    
+   })
+
+
+   //  app.get('/user/setAdmin/date',checkAuthenticated,(req, res)=>{
+   //  var user = req.session.usersChoose[0];
+  // console.log(req.session.usersChoose.length)
+//  if(req.session.usersChoose.length!=0){
+ //    var user  =  req.session.usersChoose.pop(0) //wyjmujemy + usuwamy
+ //    console.log(user)
+  //   console.log(req.session.usersChoose.length)
+ //      res.render('setAdmin.ejs',{user:user})
+ //     }
+  //    else
+  //    {
+  //      res.render('/users/overview')
+  //    }
+  //   });
 }
 module.exports =  overviewUsers
--- submits/overviewUsers.js
+++ submits/overviewUsers.js
@@ -1,27 +1,21 @@
 function submit()
 {
-    //admin 
-  var iframe = document.getElementById("ChoosedUsers");   //dobieram sie do iframe
-  var select = iframe.contentWindow.document.getElementById("choosedUsers");
-  var options = select.getElementsByTagName('option');    //pobieram opcje z listy
-if(options.length==0){
-  alert("Bad numbers of users")
-  return;
-}
-var JSONToSend ="{\"usersToAdd\": [";
-var JSONArrayOfUsersToAdd = "";
-for (i = 0; i < options.length; i++)
-{
-    JSONArrayOfUsersToAdd +="{"
-  var optionsToAdd = options[i].text.split(' ') 
-    JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd + "\"id\":\"" + optionsToAdd[0].substring(0, optionsToAdd[0].length - 1) + "\","
-    JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd + "\"email\":\"" + optionsToAdd[1] + "\", "
-    JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd + "\"lastName\":\"" + optionsToAdd[3] + "\", "
-    JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd + "\"firstName\":\"" + optionsToAdd[2] + "\"},"
-}
-JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd.substring(0, JSONArrayOfUsersToAdd.length - 1); //usunięcie przecinka
-JSONToSend = JSONToSend + JSONArrayOfUsersToAdd +"]}";
+  var iframe = document.getElementById("AvaibleUsers");   //dobieram sie do iframe
+  var select = iframe.contentWindow.document.getElementById("choosedUser");
+
+ if( select.selectedIndex ==-1)
+  alert("Select one user!")
+
+  var option = select.options[select.selectedIndex].text;    //pobieram opcje z listy
+var JSONToSend ="{\"userToAdd\": [{";
+
+  var optionsToAdd = option.split(' ') 
+    JSONToSend = JSONToSend + "\"id\":\"" + optionsToAdd[0].substring(0, optionsToAdd[0].length - 1) + "\","
+    JSONToSend = JSONToSend  + "\"email\":\"" + optionsToAdd[1] + "\", "
+    JSONToSend = JSONToSend  + "\"lastName\":\"" + optionsToAdd[3] + "\", "
+    JSONToSend = JSONToSend  + "\"firstName\":\"" + optionsToAdd[2] + "\"}]}"
+//JSONArrayOfUsersToAdd = JSONArrayOfUsersToAdd.substring(0, JSONArrayOfUsersToAdd.length - 1); //usunięcie przecinka
  console.log(JSONToSend)
-  location='/users/setAdmin?JSONFrom='+JSONToSend
+  location='/user/showStatistics?JSONFrom='+JSONToSend
 
 }
