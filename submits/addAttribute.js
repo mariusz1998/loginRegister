e299c71cced473bf79026f82e7da1436e570d421
Mariusz Drynda <mariusz1998@wp.pl>
2020-12-30 18:04:05 +0100

    atrybuty nie mogą się powatarzać przy add file

--- submits/addAttribute.js
+++ submits/addAttribute.js
@@ -2,11 +2,18 @@ function addNewAttr(toAdd)
 {
     var iframe = document.getElementById("Choosed"+toAdd);   //dobieram sie do iframe
     var select = iframe.contentWindow.document.getElementById("choosed"+toAdd);  
-   
+    var options = select.getElementsByTagName('option'); 
     var option = document.createElement("option");
     option.text =  document.getElementById("newAttribute").value;
     if(option.text.length<1)
     return;
+
+    for (i = 0; i < options.length; i++)  {
+     if( options[i].text ==option.text){
+           alert('Attribute exists!')
+           return
+        }
+    }
     select.add(option);
 
     myCountingFunction(toAdd);
