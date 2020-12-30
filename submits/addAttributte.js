67a575699de2075bba312f8d9cc899ec6cb57b2a
Mariusz Drynda <mariusz1998@wp.pl>
2020-12-30 14:56:20 +0100

    dodawanie arybutu własnego

---
+++ submits/addAttributte.js
@@ -0,0 +1,14 @@
+function addNewAttr(toAdd)
+{
+    var iframe = document.getElementById("Choosed"+toAdd);   //dobieram sie do iframe
+    var select = iframe.contentWindow.document.getElementById("choosed"+toAdd);  
+   
+    var option = document.createElement("option");
+    option.text =  document.getElementById("newAttribute").value;
+    console.log(document.getElementById("newAttribute").value)
+    if(option.text.length<1)
+    return;
+    select.add(option);
+
+    myCountingFunction(toAdd);
+}
