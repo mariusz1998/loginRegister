01d05b794f0781fea118eea1ec43ea9bd384f93a
Mariusz Drynda <mariusz1998@wp.pl>
2021-01-03 16:23:35 +0100

    pokazywanie atrybóyów w dodatkowym oknie

--- javascripts/showUserFiles.js
+++ javascripts/showUserFiles.js
@@ -1,6 +1,7 @@
 function showUserFiles(app,checkAuthenticated,sessionNeo) {
     app.post('/show/your/files',checkAuthenticated,(req, res)=>{
         var tableDataFile=""
+        var  attrFiles="["
             // req.user.email=req.body.email
              sessionNeo
                  .run('MATCH (u:User ) WHERE id(u)=$idParam OPTIONAL MATCH (f:File)<-[:OWNER]-(u) RETURN f',
@@ -14,11 +15,20 @@ function showUserFiles(app,checkAuthenticated,sessionNeo) {
                             tableDataFile +="<td>"+record.get('f').properties.localization+"</td>";
                             tableDataFile +="<td>"+record.get('f').properties.firstDay+"</td>";
                             tableDataFile +="<td>"+record.get('f').properties.lastDay+"</td></tr>";
-
+                            attrFiles+="{\"id\":" + record.get('f').identity.low +",",
+                            attrFiles +="\"arrayAttrFile\":["
+                            for(var i=0;i<record.get('f').properties.attribute.length;i++)
+                            attrFiles+= "\""+record.get('f').properties.attribute[i]+"\","
+                            attrFiles =  attrFiles.substring(0,  attrFiles.length - 1); 
+                            attrFiles+="]},"
                            // tableDataFile +="<td>"+record.get('f').properties.firstDay+"</td>";
                     })
+                    attrFiles =  attrFiles.substring(0,  attrFiles.length - 1); //usunięcie przecinka
+                    attrFiles+="]"
+                 //   var obj = JSON.parse(attrFiles);
+                 //   console.log(obj[0]["arrayAttrFile"])
                    // setTimeout(async () =>{ 
-                     res.render('userFiles/files.ejs',{tableData: tableDataFile})  
+                    res.render('userFiles/files.ejs',{tableData: tableDataFile,arrayFilesAttr:attrFiles})  
                   //  }  ,2000)
                 })
                
