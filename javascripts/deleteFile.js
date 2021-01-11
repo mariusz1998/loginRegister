a0e4a6092c91713c326788f498ab08f23bb7a79d
Mariusz Drynda <mariusz1998@wp.pl>
2021-01-11 16:22:35 +0100

    usuwanie innych plików  przez admina

--- javascripts/deleteFile.js
+++ javascripts/deleteFile.js
@@ -2,6 +2,7 @@ function deleteFile(app,checkAuthenticated,sessionNeo,auth,google) {
 
     app.get('/delete/file', (req, res) => {
         var obj = JSON.parse(req.query.JSONFrom);
+        var otherFiles = JSON.parse(req.query.otherFiles)
         console.log(obj[0]["id"]+" "+obj[0]["nameFile"])
      sessionNeo          
      .run('MATCH (n:File) Where id(n)=$idParam RETURN n.googleID as googleId',
@@ -27,7 +28,10 @@ function deleteFile(app,checkAuthenticated,sessionNeo,auth,google) {
            sessionNeo
            .run('MATCH (n:File) where id(n)=$idParam DETACH DELETE n',{idParam: obj[0]["id"]})
            .then(function(){
+             if(otherFiles==false)
              res.render('userFiles/deleteFileSucces.ejs',{information:obj[0]["id"]+" "+obj[0]["nameFile"]});
+             else
+             res.render('otherFiles/deleteOtherFileSuccess.ejs',{information:obj[0]["id"]+" "+obj[0]["nameFile"]});
          })
          }
        }
