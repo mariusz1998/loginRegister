8d6c595554849a34dd09cb56ae72b42f4f8d9d7f
Mariusz Drynda <mariusz1998@wp.pl>
2021-01-11 12:49:03 +0100

    usuwanie konta wraz z admin node

--- javascripts/deleteUserAccount.js
+++ javascripts/deleteUserAccount.js
@@ -11,9 +11,36 @@ function deleteUserAccount(app,checkAuthenticated,sessionNeo) {
               res.redirect('/logout?_method=POST');
             })
         }
-        else
+        else //is admin 
         {
-
+          var today = new Date();
+          var dd = today.getDate();
+          var mm = today.getMonth()+1; 
+          var yyyy = today.getFullYear();
+          if(dd<10) 
+            dd='0'+dd;
+          if(mm<10) 
+            mm='0'+mm; 
+            var dateToday = yyyy+"-"+mm+"-"+dd;
+          // var dateToday="2022-03-20"
+          sessionNeo
+          
+            .run(' MATCH (u:User)-[r:ADMIN]-(b:Admin) Where id(u)<>$idParam and b.endDay>=date($todayParam) and b.startDay<=date($todayParam) RETURN u'
+            ,{idParam: req.user.id,todayParam:dateToday})
+            .then(function(result){
+              console.log(result.records.length)
+              if(result.records.length == 0 || result.records[0].get('u')==null)
+              res.render('editUser/deleteUserWarning.ejs')
+              else{
+                sessionNeo
+          
+                .run('MATCH (n:User) where id(n)=$idParam OPTIONAL MATCH (n)-[r:ADMIN]-(a:Admin) where id(n)=$idParam DETACH DELETE n,r,a',{idParam: req.user.id})
+                .then(function(){
+                  res.redirect('/logout?_method=POST');
+                })
+              }
+            })
+         
 
         }
        //  res.render('editUser/editUserData.ejs',{user: req.user})
