7a6e40dbbf0c4514ac912ac7899c4b5352875880
Mariusz Drynda <mariusz1998@wp.pl>
2021-01-12 12:51:59 +0100

    pobranie pliku co ma się dostęp

--- javascripts/downloadFile.js
+++ javascripts/downloadFile.js
@@ -1,18 +1,17 @@
 function downloadFile(app,checkAuthenticated,sessionNeo,auth,formidable,fs,google) {
-   app.get('/downoad/my/file',checkAuthenticated, (req, res) => {
+   app.get('/download/file',checkAuthenticated, (req, res) => {
     var obj = JSON.parse(req.query.JSONFrom);
-console.log(obj[0]["id"])
-console.log(obj[0]["nameFile"])
+    var myFiles = JSON.parse(req.query.myFiles);
     sessionNeo          
     .run('MATCH (n:File) Where id(n)=$idParam RETURN n.googleID as googleId',
     {idParam: obj[0]["id"] }) 
               .then(result => {
             var googleID=  result.records[0].get('googleId')   
-            console.log( googleID)
+           // console.log( googleID)
             const homeDir = require('os').homedir();
-            console.log(homeDir)
+           // console.log(homeDir)
             const desktopDir = homeDir+`\\Desktop`+"\\DataLakeFiles";
-            console.log(desktopDir);
+          //  console.log(desktopDir);
             fs.promises.mkdir(desktopDir, { recursive: true })
             const dest = fs.createWriteStream(desktopDir+"/"+obj[0]["nameFile"]);
           // Authenticating drive API
@@ -22,7 +21,10 @@ console.log(obj[0]["nameFile"])
             response.data
               .on('end', () => {
                   console.log('Done');
+                  if(myFiles==true)
                   res.render('userFiles/downloadFileSucces.ejs',{path:desktopDir}); 
+                  else
+                  res.render('userAccessFiles/downloadFileSucces.ejs',{path:desktopDir}); 
               })
               .on('error', err => {
                   console.log('Error', err);
@@ -32,5 +34,6 @@ console.log(obj[0]["nameFile"])
       );
          })
              })
+             
 }
 module.exports = downloadFile
